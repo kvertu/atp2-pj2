448bdc7960ca18e22c41d9bec3c1ddf91b6324fb
kvertu <vertu_kayke@hotmail.com>
2023-11-01 18:26:53 -0300

    Entender porque a lista fica em ordem estranha

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
+#define PATH_MAX 260
 #include <stdio.h>
 #include "include/sorting.h"
 
 int main() {
     ilist l = new_list();
-    load_list("test2.bin", l);
+    load_list("test.bin", l);
     print_list(l);
     printf("\n");
+    quick_sort(l, 0, l->last, str_vemantes);
+    print_list(l);
     free_list(l);
 
     return 0;
--- sorting.c
+++ sorting.c
@@ -1,53 +1,67 @@
+#define PATH_MAX 260
 #include "include/sorting.h"
 #include <stdio.h>
 
 bool char_vemantes(char a, char b) {
-    return a < b;
+    return a > b;
 }
 
 bool str_vemantes(char * a, char * b) {
     // TODO: Refatorar esse código nojento
     int tama = strlen(a);
     int tamb = strlen(b);
+    long t = strcmp(a, b);
 
-    if (tama <= tamb) {
-        // A string a é menor ou as duas tem tamanho igual
-        for (int i = 0; i < tama; i++)
-            // Garante que os caracteres são diferentes
-            if (a[i] != b[i])
-                // Determina quem vem antes
-                if (char_vemantes(a[i], b[i]))
-                    // a[i] vem antes
-                    return true; // a vem primeiro
-                else
-                    // b[i] vem antes
-                    return false; // b vem primeiro   
-        // Se todos os caracteres verificados são iguais, então é decidido pelo tamanho
-        if (tama == tamb)
-            // Caso especial: as strings são iguais
-            // Considerando que essas strings são nomes de arquivos, deve-se garantir que não há strings repetidas na lista
-            printf("AVISO: As duas strings comparadas são iguais, retornando true.\n");
-
-        return true; // Já que tama <= tamb, a vem primeiro
+    if (t > 0) {
+        // a vem antes
+        return true;
+    } else if (t == 0) {
+        // a é igual a b
+        // Considerando que essas strings são nomes de arquivos, deve-se garantir que não há strings repetidas na lista
+        printf("AVISO: As duas strings comparadas são iguais, retornando true.\n");
+        return true;
     } else {
-        // A string b é menor
-        for (int i = 0; i < tamb; i++)
-            // Garante que os caracteres são diferentes
-            if (a[i] != b[i])
-                // Determina quem vem antes
-                if (char_vemantes(a[i], b[i]))
-                    // a[i] vem antes
-                    return true; // a vem primeiro
-                else
-                    // b[i] vem antes
-                    return false; // b vem primeiro   
-        // Se todos os caracteres verificados são iguais, então é decidido pelo tamanho
-        return false; // Já que tama > tamb, b vem primeiro
+        // b vem antes
+        return false;
     }
+    // if (tama <= tamb) {
+    //     // A string a é menor ou as duas tem tamanho igual
+    //     for (int i = 0; i < tama; i++)
+    //         // Garante que os caracteres são diferentes
+    //         if (a[i] != b[i])
+    //             // Determina quem vem antes
+    //             if (char_vemantes(a[i], b[i]))
+    //                 // a[i] vem antes
+    //                 return true; // a vem primeiro
+    //             else
+    //                 // b[i] vem antes
+    //                 return false; // b vem primeiro   
+    //     // Se todos os caracteres verificados são iguais, então é decidido pelo tamanho
+    //     if (tama == tamb)
+    //         // Caso especial: as strings são iguais
+    //         // Considerando que essas strings são nomes de arquivos, deve-se garantir que não há strings repetidas na lista
+    //         printf("AVISO: As duas strings comparadas são iguais, retornando true.\n");
+
+    //     return true; // Já que tama <= tamb, a vem primeiro
+    // } else {
+    //     // A string b é menor
+    //     for (int i = 0; i < tamb; i++)
+    //         // Garante que os caracteres são diferentes
+    //         if (a[i] != b[i])
+    //             // Determina quem vem antes
+    //             if (char_vemantes(a[i], b[i]))
+    //                 // a[i] vem antes
+    //                 return true; // a vem primeiro
+    //             else
+    //                 // b[i] vem antes
+    //                 return false; // b vem primeiro   
+    //     // Se todos os caracteres verificados são iguais, então é decidido pelo tamanho
+    //     return false; // Já que tama > tamb, b vem primeiro
+    // }
 }
 
 int separa(ilist l, int low, int up, bool (*comparar)(char *, char *)) {
-    char * piv, * aux;
+    char piv[PATH_MAX], aux[PATH_MAX];
     getvalue_list(l, l->last, piv); // Pivô
     int j = low; // Posição do particionamento
     for (int k = low; k < up; k++) {
@@ -67,5 +81,7 @@ bool quick_sort(ilist l, int low, int up, bool (*comparar)(char *, char *)) {
         j = separa(l, low, up, comparar);
         quick_sort(l, low, j - 1, comparar); // Chamada para elementos à esquerda do pivô
         quick_sort(l, j + 1, up, comparar); // Chamada para elementos à direita do pivô
+        print_list(l);
+        printf("\n");
     }
 }
