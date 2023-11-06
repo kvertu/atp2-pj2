ee017c3128c85442b39335f0d7861c4615a79c00
Kayke Guilherme Vertu <143103025+kvertu@users.noreply.github.com>
2023-11-06 20:35:17 -0300

    trocar_list finalmente corrigido
    Demorou, mas foi!

--- imagelist.c
+++ imagelist.c
@@ -102,6 +102,7 @@ bool getvalue_list(ilist l, int pos, char * val) {
 
 bool trocar_list(ilist l, int p1, int p2) {
     if (p1 < 0 || p1 > l->last || p2 < 0 || p2 > l->last) {
+        // Caso as posições estejam fora do escopo da lista
         printf("ERRO: Indices fora do escopo da lista, operação cancelada.\n");
         return false;
     }
@@ -110,34 +111,40 @@ bool trocar_list(ilist l, int p1, int p2) {
         // Se as duas posições são iguais, então não é necessário fazer nada
         return true;
 
-    image i1 = l->first;
-    image i2 = l->first;
-
-    for (int i = 1; i < p1; i++) {
-        i1 = i1->next;
-    }
-
-    for (int i = 1; i < p2; i++) {
+    image i2 = l->first; // Ponteiro para o elemento em p2
+    // Percorre a lista até o elemento antes de p2
+    for (int i = 0; i < p2 - 1; i++) {
         i2 = i2->next;
     }
 
-    // TODO: Troca de elementos não funciona quando uma das posições é zero, descobrir porque
-    image aux = i2->next;
-    image aux_next = i2->next->next;
-    
-    i2->next->next = i1->next->next;
-    i2->next = i1->next;
-    
-    i1->next->next = aux_next;
-    i1->next = aux;
+    image aux, auxn; // Ponteiros auxiliares para a troca
+    aux = i2->next;
+    auxn = i2->next->next;
 
     if (p1 == 0) {
-        i2 = i1;
-        l->first = i2;
-    }
-    if (p2 == 0) {
-        i1 = i2;
-        l->first = i1;
+        // Caso especial para p1 == 0
+        i2->next->next = l->first->next;
+        i2->next = l->first;
+
+        l->first->next = auxn;
+        l->first = aux;
+    } else if (p2 == 0) {
+        // Caso especial para p2 == 0
+        // O raciocinio é identico ao p1 == 0, então vale a pena usar recursão, invertendo as posições
+        return trocar_list(l, p2, p1);
+    } else {
+        // Caso geral
+        image i1 = l->first; // Ponteiro para o elemento em p1
+        // Percorre a lista até o elemento antes de p1
+        for (int i = 0; i < p1 - 1; i++) {
+            i1 = i1->next;
+        }
+
+        i2->next->next = i1->next->next;
+        i2->next = i1->next;
+
+        i1->next->next = auxn;
+        i1->next = aux;
     }
 
     return true;
